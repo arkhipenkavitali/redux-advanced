03b3e62f9073463f4a5b843016dae87ab990b315
vitality <qwerty@qwerty.com>
2022-08-15 15:41:49 +0300

    reducers and async reduces in toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {useAppDispatch, useAppSelector} from "./store/hooks/redux";
 import {userSlice} from "./store/reducers/UserSlice";
+import {fetchUsers} from "./store/reducers/ActionCreater";
 
 function App() {
     const dispatch = useAppDispatch();
+    const {users, isLoading, error} = useAppSelector(state => state.userReducer)
 
-
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [])
   return (
     <div className="App">
-
+        {isLoading && <p>Loading</p>}
+        {error && <p>{error}</p>}
+        {JSON.stringify(users,null,2)}
     </div>
   );
 }
