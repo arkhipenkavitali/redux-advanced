64b589c16e7211f5e27bcdcb2f301977facc5439
vitality <qwerty@qwerty.com>
2022-08-15 15:03:04 +0300

    count example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import {useAppDispatch, useAppSelector} from "./store/hooks/redux";
+import {userSlice} from "./store/reducers/UserSlice";
 
 function App() {
+    const {count} = useAppSelector(state => state.userReducer)
+    const {increment} = userSlice.actions
+    const dispatch = useAppDispatch();
+
+
   return (
     <div className="App">
-      work
+        <h1>{count}</h1>
+        <button onClick={() => dispatch(increment(5))}>Click me</button>
     </div>
   );
 }
