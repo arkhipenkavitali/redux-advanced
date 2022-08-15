52839c56696c20108455739814b9d0f1699eb0dd
vitality <qwerty@qwerty.com>
2022-08-15 15:03:56 +0300

    clear count

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,12 @@ import {useAppDispatch, useAppSelector} from "./store/hooks/redux";
 import {userSlice} from "./store/reducers/UserSlice";
 
 function App() {
-    const {count} = useAppSelector(state => state.userReducer)
-    const {increment} = userSlice.actions
     const dispatch = useAppDispatch();
 
 
   return (
     <div className="App">
-        <h1>{count}</h1>
-        <button onClick={() => dispatch(increment(5))}>Click me</button>
+
     </div>
   );
 }
